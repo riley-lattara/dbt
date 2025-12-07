1086e19161ddac6086fef3f36a5321f6be4220ae
wizard7558 <56692568+wizard7558@users.noreply.github.com>
2025-12-07 08:36:26 -0800

    Add account_id to LinkedIn campaign groups staging

--- models/staging/linkedin/v_stg_linkedin_campaign_groups.sql
+++ models/staging/linkedin/v_stg_linkedin_campaign_groups.sql
@@ -1,4 +1,5 @@
 select id
          , name
+         , account_id
     from FIVETRAN_DATABASE.LINKEDIN_ADS.CAMPAIGN_GROUP_HISTORY
     qualify row_number() over(partition by id order by last_modified_time desc) = 1
